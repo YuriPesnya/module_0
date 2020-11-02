8dd3f47a942f9087007c45fc018ed7ffde1a906a
YuriPesnya <ypesnya@gmail.com>
2020-11-02 15:46:44 +0300

    Проверяем второй пример.

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+# Проверяем работу второго алгоритма из прмера
+
 import numpy as np
 
 
-def game_core_v1(number):
-    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
        Функция принимает загаданное число и возвращает число попыток'''
-    count = 0
-    while True:
+    count = 1
+    predict = np.random.randint(1,101)
+    while number != predict:
         count+=1
-        predict = np.random.randint(1,101) # предполагаемое число
-        if number == predict:
-            return(count) # выход из цикла, если угадали
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+    return(count) # выход из цикла, если угадали
+
 
 def score_game(game_core):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
@@ -19,9 +25,10 @@ def score_game(game_core):
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
+
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return(score)
 
 # запускаем
-score_game(game_core_v1)
+score_game(game_core_v2)
 
